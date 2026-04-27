5b598edf84230069f28c1d709b719480802d901a
klas <klas.flodqvist@rebtel.com>
2026-04-27 21:04:54 +0200

    fix(test): initialize Firebase mock before pumping BeastBlocksApp in widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core_platform_interface/test.dart'
+    show setupFirebaseCoreMocks;
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:beastris/main.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  setupFirebaseCoreMocks();
+
+  setUpAll(() async {
+    await Firebase.initializeApp();
+  });
+
   testWidgets('App starts and shows title', (WidgetTester tester) async {
     await tester.pumpWidget(const BeastBlocksApp());
     expect(find.text('🐾 BEASTBLOCKS 🐾'), findsOneWidget);
